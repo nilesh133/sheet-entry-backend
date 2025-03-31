d3cda33bd318e2f8f061a54b3d2d83561589deac
Nilesh Prajapati <85280253+nilesh133@users.noreply.github.com>
2025-03-31 22:04:31 +0530

    first-commit

--- config/googleSheets.js
+++ config/googleSheets.js
@@ -1,7 +1,12 @@
 const { google } = require("googleapis");
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: "google-service-account.json",
+    credentials: {
+        type: "service_account",
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Fix newlines
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
