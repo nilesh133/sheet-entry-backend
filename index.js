7b118cc1f2c1c67a1b4d42a32a8168c343a3d658
Nilesh Prajapati <85280253+nilesh133@users.noreply.github.com>
2025-03-31 21:46:12 +0530

    first-commit

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.use(express.json());
 const formRoutes = require("./routes/formRoutes");
 app.use("/api", formRoutes);
 
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
